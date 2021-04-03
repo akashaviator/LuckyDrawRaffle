c6a7d80d248c5b9b229ecd6c127ce1451a9fb13b
= <kaushiksri0908@gmail.com>
2021-04-03 20:49:35 +0530

    admin: Add models to Django Admin.

--- lucky_draw_api/admin.py
+++ lucky_draw_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from lucky_draw_api import models
 
 # Register your models here.
+admin.site.register(models.RaffleTicket)
+admin.site.register(models.LuckyDrawRaffle)
