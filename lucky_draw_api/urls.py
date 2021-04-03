bfc2dcd6989da0e46177f5baf6d6dbe6c82f6258
= <kaushiksri0908@gmail.com>
2021-04-03 17:51:19 +0530

    views: Add GetTicketView api view.
    This adds the GetTicketView api view to server the `GET /ticket`
    endpoint to generate a new ticket for a user.

--- lucky_draw_api/urls.py
+++ lucky_draw_api/urls.py
@@ -1,2 +1,6 @@
+from django.urls import path, include
+from .views import GetTicketView
+
 urlpatterns = [
+    path('ticket', GetTicketView.as_view()),
 ]
--- lucky_draw_api/views.py
+++ lucky_draw_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import RaffleTicket
 
-# Create your views here.
+from rest_framework import request
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class GetTicketView(APIView):
+
+    # Allow GET request for any user and POST requests by
+    # authenticated users only.
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, format=None):
+        """
+        Creates a new raffle ticket and assigns the requesting user to it.
+        """
+
+        new_ticket = RaffleTicket()
+        new_ticket.player = request.user
+        new_ticket.save()
+
+        return Response({"result": "successful", "ticket_id": new_ticket.id})
